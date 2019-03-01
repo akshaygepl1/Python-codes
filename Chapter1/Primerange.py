d63f2659d709b4fc705ff243878a2ea1980c879e
akshaygepl1 <39396855+akshaygepl1@users.noreply.github.com>
2019-03-01 09:00:24 +0530

    made changes for the range operation

--- Chapter1/Primerange.py
+++ Chapter1/Primerange.py
@@ -1,10 +1,20 @@
 # Prime numbers within the given range
 
+# take input from the user
 n = int(input("Accept the first number"))
 n1 = int(input("Enter the second number"))
 
-for i in range(n,n1):
-    if i % 2 == 0 or i == 1:
-        print(i," is not prime number")
-    else :
-        print(i ,"Is prime number")
+
+# prime numbers are greater than 1
+print("Prime numbers between",n,"and",n1,"are:")
+
+for num in range(n,n1 + 1):
+   # prime numbers are greater than 1
+   if num > 1:
+       for i in range(2,num):
+           if (num % i) == 0:
+               break
+       else:
+           print(num)
+            
+            
