074b90de39fbd7e844fe87f5be531ccb770126d1
akshaygepl1 <39396855+akshaygepl1@users.noreply.github.com>
2019-03-11 13:26:37 +0530

    demo file

---
+++ Chapter1/Demonstrate.py
@@ -0,0 +1,30 @@
+#Demonstrations
+
+a= "Varsha "
+b= "Mali "
+#String contcatenation
+c= a+b
+print(c)
+#repetation of word
+print(c*4)
+
+a="hello"
+b=a.capitalize()
+print(b)
+print(a.upper())
+
+a="hello varsha , is your full name varsha Mali"
+c=a.count("varsha")
+print(c)
+
+#find function operation
+a="hello varsha , is your full name varsha Mali"
+c=a.find("varsha",15,len(a) )
+print(c)
+
+a= 10 
+b= 20
+if a<b :
+	print(a)
+else:
+	print(b)
